056e8c0217e3a2df52956a158c3cab7440077528
chaudharyjatin115 <chaudharyjatin116@proton.me>
2026-09-17 17:00:23 +0530

    update application id to com maxrave retropod and rename application class

--- androidApp/src/main/java/com/maxrave/simpmusic/SimpMusicApplication.kt
+++ androidApp/src/main/java/com/maxrave/simpmusic/SimpMusicApplication.kt
@@ -17,42 +17,29 @@ import coil3.request.CachePolicy
 import coil3.request.crossfade
 import com.maxrave.common.AppIdentity
 import com.maxrave.data.di.loader.loadAllModules
-import com.maxrave.domain.manager.DataStoreManager
 import com.maxrave.logger.Logger
 import com.maxrave.simpmusic.di.viewModelModule
-import com.maxrave.simpmusic.service.backup.AutoBackupScheduler
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.launch
 import multiplatform.network.cmptoast.AppContext
 import okhttp3.OkHttpClient
 import okio.FileSystem
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.component.KoinComponent
-import org.koin.core.component.inject
 import org.koin.core.context.loadKoinModules
 import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
-import org.simpmusic.crashlytics.configCrashlytics
 import java.lang.reflect.Field
 
-class SimpMusicApplication :
+class RetroPodApplication :
     Application(),
     KoinComponent,
     SingletonImageLoader.Factory {
 
-    private val applicationScope = CoroutineScope(SupervisorJob() + Dispatchers.Default)
-    private val dataStoreManager: DataStoreManager by inject()
-    private lateinit var autoBackupScheduler: AutoBackupScheduler
-
     override fun onCreate() {
         super.onCreate()
-        configCrashlytics(this, BuildKonfig.sentryDsn)
         startKoin {
             androidLogger(level = Level.DEBUG)
-            androidContext(this@SimpMusicApplication)
+            androidContext(this@RetroPodApplication)
             loadAllModules(
                 AppIdentity(
                     applicationId = BuildConfig.APPLICATION_ID,
@@ -62,33 +49,25 @@ class SimpMusicApplication :
             )
             loadKoinModules(viewModelModule)
         }
-        // provide custom configuration
         val workConfig =
             Configuration
                 .Builder()
                 .setMinimumLoggingLevel(Log.INFO)
                 .build()
 
-        // initialize WorkManager
         WorkManager.initialize(this, workConfig)
-
-        // Initialize and start AutoBackupScheduler
-        autoBackupScheduler = AutoBackupScheduler(this, dataStoreManager)
-        applicationScope.launch {
-            autoBackupScheduler.observeAndSchedule()
-        }
 
         CaocConfig.Builder
             .create()
-            .backgroundMode(CaocConfig.BACKGROUND_MODE_SILENT) // default: CaocConfig.BACKGROUND_MODE_SHOW_CUSTOM
-            .enabled(true) // default: true
-            .showErrorDetails(true) // default: true
-            .showRestartButton(true) // default: true
+            .backgroundMode(CaocConfig.BACKGROUND_MODE_SILENT)
+            .enabled(true)
+            .showErrorDetails(true)
+            .showRestartButton(true)
             .errorDrawable(R.mipmap.ic_launcher_round)
-            .logErrorOnRestart(false) // default: true
-            .trackActivities(true) // default: false
-            .minTimeBetweenCrashesMs(2000) // default: 3000 //default: bug image
-            .restartActivity(MainActivity::class.java) // default: null (your app's launch activity)
+            .logErrorOnRestart(false)
+            .trackActivities(true)
+            .minTimeBetweenCrashesMs(2000)
+            .restartActivity(MainActivity::class.java)
             .apply()
 
         @SuppressLint("DiscouragedPrivateApi")
@@ -104,7 +83,6 @@ class SimpMusicApplication :
 
     override fun onTerminate() {
         super.onTerminate()
-
         Logger.w("Terminate", "Checking")
     }
 
